5c2a953b450572b4d5438556125570aa13468835
Erfan Goudarzi <erfangoudarzi@gmail.com>
2023-03-11 20:31:37 -0800

    Adding the list To Java

---
+++ JavaScript/Java.js
@@ -0,0 +1,5 @@
+const courseList = [
+    { code: "ACIT 1620", name: "Fundamental Web Technologies" },
+    { code: "ACIT 1515", name: "Scripting for IT" },
+    { code: "ACIT 1420", name: "System Administration" },
+  ];
