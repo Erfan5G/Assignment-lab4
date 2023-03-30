22ed21d78ee091542827b459cfb642394266eb7f
Erfan Goudarzi <erfangoudarzi@gmail.com>
2023-03-30 13:58:31 -0700

    bringing back the previous Js codes

--- JavaScript/Java.js
+++ JavaScript/Java.js
@@ -1,3 +1,33 @@
+//==============-----------------------------| Previous Js codes |--------------------------------------=================
+
+// const courseList = [
+//   { code: "ACIT 1620", name: "Fundamental Web Technologies" },
+//   { code: "ACIT 1515", name: "Scripting for IT" },
+//   { code: "ACIT 1420", name: "System Administration" },
+// ];
+
+// let userNumber = prompt("Enter a 4-digit number:");
+
+// while (userNumber.length !== 4 || isNaN(userNumber)) {
+//   userNumber = prompt("Invalid input. Please enter a 4-digit number:");
+// };
+
+// let courseFound = false;
+
+// for (let i = 0; i < courseList.length; i++) {
+//   if (courseList[i].code.includes(userNumber)) {
+//       console.log(`Yes, I am taking the course: ${courseList[i].code} --> ${courseList[i].name}`);
+//     courseFound = true;
+//     break;
+//   }
+// };
+
+// if (!courseFound) {
+//   const newCourse = { code: `ACIT ${userNumber}`, name: null };
+//   courseList.push(newCourse);
+//   console.log(`Added new course: ${newCourse.code}`);
+// }; 
+
 //------------------------------------| Create courseArray Function |-----------------------------------------------------
 function createCourseArray() {
   const courseElements = document.querySelectorAll('.courses');
